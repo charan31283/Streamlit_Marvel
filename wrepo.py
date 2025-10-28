ef9c08ec8ac4c934fc1d5bf3c8bb6335fbb188ce
PALAM MADHU CHARAN REDDY <110419584+charan31283@users.noreply.github.com>
2025-10-28 18:50:03 +0530

    Add files via upload

---
+++ wrepo.py
@@ -0,0 +1,44 @@
+
+import requests as r
+import streamlit as st
+from google import genai
+
+st.set_page_config(page_title="Weather & Safety Assistant")
+
+st.title("☁️ Weather & Safety Assistant")
+st.write("Enter your **Latitude** and **Longitude** to get live weather info and precautions!")
+
+lat = st.number_input("Enter Latitude:", format="%.6f")
+lon = st.number_input("Enter Longitude:", format="%.6f")
+
+weather_api_key = "8f1b2bb4e9921443522d43cc36a8a719"
+gemini_api_key = "AIzaSyD2k4du2yV_ce2X2_xf8ohXCHPp68S9UD0"
+
+if st.button("Get Weather & Advice"):
+    if lat == 0 and lon == 0:
+        st.warning("⚠️ Please enter valid coordinates.")
+    else:
+        
+            url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_api_key}"
+            response = r.get(url)
+            data = response.json()
+
+            if response.status_code != 200 or "main" not in data:
+                st.error("❌ Could not fetch weather details. Check coordinates.")
+            else:
+                tk = data["main"]["temp"]
+                tc = round(tk - 273.15, 2)
+                city = data.get("name", "your location")
+
+                st.success(f"The current temperature is {tc}°C in {data['name']} with {data['weather'][0]['main']} and humidity of {data['main']['humidity']}% .Tell me some precatuions")
+
+                query = f"The current temperature is {tc}°C in {city}. Suggest some precautions."
+                client = genai.Client(api_key=gemini_api_key)
+                ai_response = client.models.generate_content(
+                    model="gemini-2.5-flash", contents=query
+                )
+
+                st.subheader("🌤️ Precautionary Advice:")
+                st.write(ai_response.text)
+
+        
